9f16d1bc8d19e5e17256cd3e440f3eb044321f90
杉野正樹 <himazin0606@gmail.com>
2021-03-27 10:27:54 +0900

    デプロイ後のバグ修正（スペルミス）

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ApplicationRecord
   has_rich_text :content
   belongs_to :user
   acts_as_taggable
-  default_scope->{order(creates_at: :desc)}
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true
 
